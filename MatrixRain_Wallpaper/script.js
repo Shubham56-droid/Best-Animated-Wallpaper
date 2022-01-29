708295a50312d0259b009aabdfe0fd96198a37c3
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2022-01-30 00:05:53 +0530

    Add files via upload

---
+++ MatrixRain_Wallpaper/script.js
@@ -0,0 +1,50 @@
+const canvas  = document.getElementById('canvas1');
+const ctx = canvas.getContext('2D');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol{
+    constructor(x,y,fontSize,canvasHeight){
+        this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = '';
+        this.canvasHeight = canvasHeight;
+    }
+    draw(){
+        this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
+        context.fillStyle = '#0aff0a';
+        context.fillStyle(this.text,this.x * this.fontSize,this.y * this.fontSize);
+        if(this.y * this.fontSize > this.canvasHeight){
+            this.y = 0;
+        }else{
+            this.y += 1;
+        }
+    }
+}
+
+class Effect{
+    constructor(canvasWidth,canvasHeight){
+        this.canvasWidth = canvas.width;
+        this.canvasHeight = canvas.height;
+        this.fontSize = 25;
+        this.columns = this.canvasWidth / this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+    }
+    #initialize(){
+        for (let i = 0; i < this.columns.length; i++) {
+            this.symbols[i] = new Symbol(i,0,this.fontSize,this.canvasHeight);
+        }
+    }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+
+function animate(){
+    ctx.font = effect.fontSize + 'px monospace';
+    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    requestAnimationFrame(animate);
+}
+animate();
