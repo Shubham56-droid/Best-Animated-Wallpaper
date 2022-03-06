52c636217c6daffb0265fb7b4609fe45329b8198
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2022-03-07 00:24:09 +0530

    Add files via upload

--- ShootingStarBackground/script.js
+++ ShootingStarBackground/script.js
@@ -1,9 +1,10 @@
 let bg = document.getElementById('bgImg');
-   
+    const backbg = ['bg1','bg2','bg3'];
+    
     let i = 0;
     function animate(num)
     {
-        bg.style.backgroundImage = 'url(./images/bg'+ `${num}` + '.jpg)';
+        bg.style.backgroundImage = 'url(./images/'+ `${backbg[num]}` + '.jpg)';
         bg.style.transition = '2s';
         i++;
         if(i==backbg.length)
