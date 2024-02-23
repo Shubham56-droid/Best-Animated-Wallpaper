2ff5444a44736b92cbfcd5f7fec2dcc26d512e7b
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2024-02-23 11:18:47 +0530

    Add files via upload

--- MatrixRain/script.js
+++ MatrixRain/script.js
@@ -1,50 +1,72 @@
-const canvas  = document.getElementById('canvas1');
-const ctx = canvas.getContext('2D');
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Symbol{
-    constructor(x,y,fontSize,canvasHeight){
-        this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        this.x = x;
-        this.y = y;
-        this.fontSize = fontSize;
-        this.text = '';
-        this.canvasHeight = canvasHeight;
+class Symbol {
+  constructor(x, y, fontSize, canvasHeight) {
+    this.charecters =
+      "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.text = "";
+    this.canvasHeight = canvasHeight;
+  }
+  draw(context) {
+    this.text = this.charecters.charAt(
+      Math.floor(Math.random() * this.charecters.length)
+    );
+    context.fillStyle = "#0aff0a";
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+      this.y = 0;
+    } else {
+      this.y += 1;
     }
-    draw(){
-        this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
-        context.fillStyle = '#0aff0a';
-        context.fillStyle(this.text,this.x * this.fontSize,this.y * this.fontSize);
-        if(this.y * this.fontSize > this.canvasHeight){
-            this.y = 0;
-        }else{
-            this.y += 1;
-        }
-    }
+  }
 }
 
-class Effect{
-    constructor(canvasWidth,canvasHeight){
-        this.canvasWidth = canvas.width;
-        this.canvasHeight = canvas.height;
-        this.fontSize = 25;
-        this.columns = this.canvasWidth / this.fontSize;
-        this.symbols = [];
-        this.#initialize();
+class Effect {
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasHeight = canvasHeight;
+    this.canvasWidth = canvasWidth;
+    this.fontSize = 25;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+    console.log(this.symbols);
+  }
+  #initialize() {
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i,0, this.fontSize, this.canvasHeight);
     }
-    #initialize(){
-        for (let i = 0; i < this.columns.length; i++) {
-            this.symbols[i] = new Symbol(i,0,this.fontSize,this.canvasHeight);
-        }
-    }
+  }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 15;
+const nextFrame = 1000/fps;
+let timer = 0;
 
-function animate(){
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
-    requestAnimationFrame(animate);
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if(timer > nextFrame){
+    ctx.fillStyle = 'rgba(0,0,0,0.05)';
+    ctx.textAlign = 'center';
+    // ctx.fillStyle = '#fff';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    ctx.font = effect.fontSize + "px monospace";
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+    timer = 0;
+  }
+  else
+  {
+    timer += deltaTime;
+  }
+  requestAnimationFrame(animate);
 }
-animate();
+animate(0);
